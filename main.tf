602a0edc68618ae80ad1cbca948d209f92407ffd
sravanth <sravanth.alluri@outlook.com>
2026-06-27 17:34:11 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+# -----------------------------
+# VPC Network
+# -----------------------------
+resource "google_compute_network" "vpc" {
+  name                    = "tf-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "tf-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+  ip_cidr_range = "10.10.0.0/24"
+}
+
+# -----------------------------
+# Firewall Rule (SSH)
+# -----------------------------
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# -----------------------------
+# VM Instance
+# -----------------------------
+resource "google_compute_instance" "vm" {
+  name         = "tf-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 20
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc.id
+    subnetwork = google_compute_subnetwork.subnet.id
+
+    access_config {}  # creates external IP
+  }
+
+  metadata = {
+    ssh-keys = "sravanth:${file(var.public_key_path)}"
+  }
+
+  tags = ["ssh"]
+}
