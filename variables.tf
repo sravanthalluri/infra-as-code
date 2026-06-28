2884438cb91ff72556845ed37a8e1e312d58c2e9
sravanth <sravanth.alluri@outlook.com>
2026-06-28 16:42:54 -0400

    second commit changes to variable files

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "machine_type" {
+  description = "Compute Engine machine type"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "image" {
+  description = "Boot disk image"
+  type        = string
+  default     = "debian-cloud/debian-12"
+}
+
+variable "disk_size" {
+  description = "Boot disk size in GB"
+  type        = number
+  default     = 20
+}
+
+variable "ssh_user" {
+  description = "SSH username"
+  type        = string
+  default     = "sravanth-ssh-key"
+}
+
+variable "public_key_path" {
+  description = "Path to your SSH public key"
+  type        = string
+}
